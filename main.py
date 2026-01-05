640e2bac1e83560ece1bb74d0a342178236afc8b
Toy085 <brightabaka1@gmail.com>
2026-01-05 18:51:40 +0100

    Refactor password submission logic and add dedicated password check function

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
+# Application window for master password input
 root = tk.Tk()
 root.title("Your Password Manager")
 root.geometry("300x200")
 
-# Label
 label = ttk.Label(root, text="Type in your master password:")
 label.pack(pady=10)
 
@@ -15,15 +15,19 @@ password_entry.pack(pady=10)
 
 def submit_password():
     master_password = password_entry.get()
-    if master_password == "1234":
+    if check_master_password(master_password):
         open_main_window()
         root.withdraw()
     else:
         messagebox.showerror("Error", "Incorrect password")
 
+def check_master_password(string) -> bool:
+    return string == "1234"
+
 submit_button = ttk.Button(root, text="Submit", command=submit_password)
 submit_button.pack(pady=10)
 
+# Function to open the main application window
 def open_main_window():
     main_window = tk.Toplevel(root)
     main_window.title("Password Manager")
