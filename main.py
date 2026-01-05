efce193858099ba059153c77fcff07384897531b
Toy085 <brightabaka1@gmail.com>
2026-01-05 19:14:10 +0100

    Refactor master password input window setup for improved readability

--- main.py
+++ main.py
@@ -5,25 +5,9 @@ import hashlib
 import base64
 from cryptography.fernet import Fernet
 
+# Logic Functions
 def master_password_exists() -> bool:
     return os.path.exists("master.key")
-
-# Application window for master password input
-root = tk.Tk()
-root.title("Your Password Manager")
-root.geometry("300x200")
-
-label = ttk.Label(root, text="Type in your master password:")
-
-if master_password_exists():
-    label.config(text="Type in your master password:")
-else:
-    label.config(text="Create a master password:")
-
-label.pack(pady=10)
-
-password_entry = ttk.Entry(root, show="*")
-password_entry.pack(pady=10)
 
 def hash_master_password(password: str) -> bytes:
     salt = os.urandom(16)
@@ -56,7 +40,6 @@ def check_master_password(password: str) -> bool:
 
     return new_key == stored_key
 
-
 def submit_password():
     master_password = password_entry.get()
 
@@ -75,6 +58,24 @@ def submit_password():
         messagebox.showinfo("Success", "Master password created!")
         open_main_window()
         root.withdraw()
+
+# Application window for master password input
+root = tk.Tk()
+root.title("Your Password Manager")
+root.geometry("300x200")
+
+label = ttk.Label(root, text="Type in your master password:")
+
+if master_password_exists():
+    label.config(text="Type in your master password:")
+else:
+    label.config(text="Create a master password:")
+
+label.pack(pady=10)
+
+password_entry = ttk.Entry(root, show="*")
+password_entry.pack(pady=10)
+
 
 submit_button = ttk.Button(root, text="Submit", command=submit_password)
 submit_button.pack(pady=10)
